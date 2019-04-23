471e552ed21b92e9073045fcc8fa3e214c885791
igarashijyri <fourleafclover688@gmail.com>
2019-04-23 16:45:09 +0900

    ユーザ登録情報の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # サインインした時の遷移先
   def after_sign_in_path_for(resource)
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
    new_user_session_path
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 
 end
