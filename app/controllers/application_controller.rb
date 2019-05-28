c19be7175497c1c9a94ead860e0e60770441ebfd
igarashijyri <fourleafclover688@gmail.com>
2019-05-28 11:15:02 +0900

    バグ解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :user_logged_in?
+  before_action :profile_exit?
 
   # サインインした時の遷移先
   def after_sign_in_path_for(resource)
-    if UserProfile.find_by(user_id: current_user.id)
-      user_profiles_path
-    else
-      new_user_profile_path
-    end
+    user_profiles_path
   end
 
   # サインアウトした時の遷移先
@@ -17,9 +14,17 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  # ログイン状況判定
   def user_logged_in?
     unless  user_signed_in?
       redirect_to new_user_session_path
+    end
+  end
+
+  def profile_exit?
+    return unless current_user.blank?
+    unless UserProfile.find_by(id: current_user.id)
+      redirect_to new_user_profile_path
     end
   end
 
